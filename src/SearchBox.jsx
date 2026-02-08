6626fd33e7446b6c51b52e7339046d2615f92c5f
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-09 00:31:15 +0530

    add api

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,10 +3,25 @@ import Button from '@mui/material/Button';
 import './SearchBox.css';
 import { useState } from 'react';
 export default function SearchBox(){
+    let[city,setCity]=useState("");
     let API_URL="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
     const API_KEY="3a07e7850d5b11228e3fc77250cb9a6c";
 
-    let[city,setCity]=useState("");
+    let getWhetherInfo=async()=>{
+       let response= await fetch(`${API_URL}${city}&appid=${API_KEY}`);
+       let jsonResponse=await response.json();
+       let result={
+        temp:jsonResponse.main.temp,
+        tempMin:jsonResponse.main.temp_min,
+        tempMax:jsonResponse.main.temp_max,
+        feelsLike:jsonResponse.main.feels_like, 
+        humidity:jsonResponse.main.humidity,
+        weather:jsonResponse.weather[0].description,
+       }
+       console.log(result);
+    }
+
+    
 
     let handleChange=(evt)=>{
         setCity(evt.target.value);
@@ -16,6 +31,7 @@ export default function SearchBox(){
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWhetherInfo();
     }
     return <div className='SearchBox'>
         <h3>Search for the weather</h3>
